c327e38ce6c5fcc56d1b1a7ec1d193879f10bb53
dpromstk2000-lab <dpromstk2000@gmail.com>
2026-07-16 14:34:13 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // DPRO ケーキ・洋菓子店 LINE
 // STEP CAKE-4 public configuration
 window.DPRO_CAKE_CONFIG = Object.freeze({
-  version: "CAKE-4-CUSTOMER-BOOKING-UI-20260716",
+  version: "CAKE-9-FINAL-PRACTICAL-AUDIT-20260716",
   apiBase: "https://dpro-cake-line-api.dpromstk2000.workers.dev",
   shopCode: "dpro_cake_demo",
   liffId: "",
